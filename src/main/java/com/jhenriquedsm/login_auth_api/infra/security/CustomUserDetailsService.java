c2c3470a95fef5a95330d0d0c698214832fc8806
jhenriquedsm <jhsilvamata@gmail.com>
2024-09-02 18:23:39 -0300

    user details

---
+++ src/main/java/com/jhenriquedsm/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.jhenriquedsm.login_auth_api.infra.security;
+
+import com.jhenriquedsm.login_auth_api.domain.user.User;
+import com.jhenriquedsm.login_auth_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
